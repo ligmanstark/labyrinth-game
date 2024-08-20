04ed52b540cd182ae17a94cebf9ef49e14cd4d52
levis <miduwaystark@gmail.com>
2024-08-20 17:21:17 +0400

    modified:   src/game/main.ts

--- src/game/main.ts
+++ src/game/main.ts
@@ -17,7 +17,7 @@ const config: Phaser.Types.Core.GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
+            debug: true,
         },
     },
     scale: {
